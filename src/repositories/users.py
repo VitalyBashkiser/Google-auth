215a06cdc67dadb65212b749abb9d5e81b684ed1
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2024-11-06 20:05:09 +0200

    fix args kwargs

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,6 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.models.users import User
 from src.utils.repository import SQLAlchemyRepository
 
 
 class UsersRepository(SQLAlchemyRepository):
-    model = User
+    def __init__(self, session: AsyncSession):
+        self.model = User
+        super().__init__(session)
