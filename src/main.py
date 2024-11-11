58d798c165a4a2ed142b4b7535281c2d1ce95079
Vitalii <vitalikbashkiser@gmail.com>
2024-11-11 15:31:44 +0200

    Transferred exception imports to new file, create DRY function with all of custom exceptions with a common function for catching

--- src/main.py
+++ src/main.py
@@ -2,40 +2,11 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.api.main_routers import main_router
-from src.exceptions.error_handler import ExceptionHandlerMiddleware
-from src.exceptions.errors import (
-    UserNotFoundError,
-    UserAlreadyExistsError,
-    InvalidCredentialsError,
-    EmailNotConfirmedError,
-    InvalidTokenError,
-    PasswordResetError,
-    EmailSendError,
-    UserNotAuthenticatedError,
-)
-from src.exceptions.handlers import (
-    user_not_found_handler,
-    user_already_exists_handler,
-    invalid_credentials_handler,
-    email_not_confirmed_handler,
-    invalid_token_handler,
-    handle_password_reset_error,
-    email_send_error_handler,
-    user_not_authenticated_handler,
-)
+from src.api.main_handlers import setup_handlers
 
 app = FastAPI(title="Authorisation via Email")
 
-app.add_exception_handler(UserNotFoundError, user_not_found_handler)
-app.add_exception_handler(UserAlreadyExistsError, user_already_exists_handler)
-app.add_exception_handler(InvalidCredentialsError, invalid_credentials_handler)
-app.add_exception_handler(EmailNotConfirmedError, email_not_confirmed_handler)
-app.add_exception_handler(InvalidTokenError, invalid_token_handler)
-app.add_exception_handler(PasswordResetError, handle_password_reset_error)
-app.add_exception_handler(EmailSendError, email_send_error_handler)
-app.add_exception_handler(UserNotAuthenticatedError, user_not_authenticated_handler)
-app.add_middleware(ExceptionHandlerMiddleware)
-
+setup_handlers(app)
 
 app.include_router(main_router)
 
